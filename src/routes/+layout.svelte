4d4cce54b7ed5807c91f3b3d4e60cdbc93e3ff42
Jake Gibson <jake@j3.io>
2023-08-09 21:11:29 -0600

    initial

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	// The ordering of these imports is critical to your app working properly
+	import '../theme.postcss';
+	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+	// Most of your app wide CSS should be put in this file
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import logo from '$lib/assets/forge-icon.png';
+</script>
+
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<img alt="Forge Foundation Logo. Flame" style="height:60px" src={logo} />
+
+				<strong class="text-xl">Forge Utah Foundation</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://join.slack.com/t/forgeutah/shared_invite/zt-pietaeqb-HetfD2OIzn1RHtDtV~CH5g"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Slack
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://www.meetup.com/pro/forge-utah/"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Meetups
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://twitter.com/ForgeUtah"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Twitter
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://github.com/forgeutah"
+					target="_blank"
+					rel="noreferrer"
+				>
+					GitHub
+				</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,144 @@
+<script lang="ts">
+	import engleadership from '$lib/assets/engleadership.webp';
+	import mlops from '$lib/assets/mlops.webp';
+	import kubernetes from '$lib/assets/utahkubernetes.webp';
+</script>
+
+<div class="container h-full mx-auto">
+	<h1 class="h1">Upcoming Utah Dev Events</h1>
+	<h1 class="h1">Forge Meetups</h1>
+	<div class="grid md:grid-cols-3 gap-4">
+		<a href="https://www.meetup.com/engineering-leadership-utah/" class="card overflow-hidden">
+			<header>
+				<img alt="Engineering leadership meetup header of a mountain" src={engleadership} />
+			</header>
+			<div class="card-body">
+				<p>
+					Engineering Leadership is a meetup for engineering leaders to share ideas, learn from each
+					other, and build a community of practice around leadership.
+				</p>
+			</div>
+			<div class="card-footer">
+				<button class="btn btn-sm variant-primary">Learn More</button>
+			</div>
+		</a>
+
+		<a href="https://www.meetup.com/engineering-leadership-utah/" class="card overflow-hidden">
+			<header>
+				<img alt="Engineering leadership meetup header of a mountain" src={mlops} />
+			</header>
+			<div class="card-body">
+				<p>
+					Engineering Leadership is a meetup for engineering leaders to share ideas, learn from each
+					other, and build a community of practice around leadership.
+				</p>
+			</div>
+			<div class="card-footer">
+				<button class="btn btn-sm variant-primary">Learn More</button>
+			</div>
+		</a>
+
+		<a href="https://www.meetup.com/engineering-leadership-utah/" class="card overflow-hidden">
+			<header>
+				<img
+					class="aspect-video"
+					alt="Engineering leadership meetup header of a mountain"
+					src={kubernetes}
+				/>
+			</header>
+			<div class="card-body">
+				<p>
+					Engineering Leadership is a meetup for engineering leaders to share ideas, learn from each
+					other, and build a community of practice around leadership.
+				</p>
+			</div>
+			<div class="card-footer">
+				<button class="btn btn-sm variant-primary">Learn More</button>
+			</div>
+		</a>
+	</div>
+	<h1 class="h1">Lost Sheep Meetups</h1>
+	<div class="grid md:grid-cols-3 gap-4">
+		<a href="https://www.meetup.com/engineering-leadership-utah/" class="card overflow-hidden">
+			<header>
+				<img alt="Engineering leadership meetup header of a mountain" src={engleadership} />
+			</header>
+			<div class="card-body">
+				<p>
+					Engineering Leadership is a meetup for engineering leaders to share ideas, learn from each
+					other, and build a community of practice around leadership.
+				</p>
+			</div>
+			<div class="card-footer">
+				<button class="btn btn-sm variant-primary">Learn More</button>
+			</div>
+		</a>
+
+		<a href="https://www.meetup.com/engineering-leadership-utah/" class="card overflow-hidden">
+			<header>
+				<img alt="Engineering leadership meetup header of a mountain" src={mlops} />
+			</header>
+			<div class="card-body">
+				<p>
+					Engineering Leadership is a meetup for engineering leaders to share ideas, learn from each
+					other, and build a community of practice around leadership.
+				</p>
+			</div>
+			<div class="card-footer">
+				<button class="btn btn-sm variant-primary">Learn More</button>
+			</div>
+		</a>
+
+		<a href="https://www.meetup.com/engineering-leadership-utah/" class="card overflow-hidden">
+			<header>
+				<img
+					class="aspect-video"
+					alt="Engineering leadership meetup header of a mountain"
+					src={kubernetes}
+				/>
+			</header>
+			<div class="card-body">
+				<p>
+					Engineering Leadership is a meetup for engineering leaders to share ideas, learn from each
+					other, and build a community of practice around leadership.
+				</p>
+			</div>
+			<div class="card-footer">
+				<button class="btn btn-sm variant-primary">Learn More</button>
+			</div>
+		</a>
+	</div>
+</div>
+
+<style lang="postcss">
+	figure {
+		@apply flex relative flex-col;
+	}
+	figure svg,
+	.img-bg {
+		@apply w-64 h-64 md:w-80 md:h-80;
+	}
+	.img-bg {
+		@apply absolute z-[-1] rounded-full blur-[50px] transition-all;
+		animation: pulse 5s cubic-bezier(0, 0, 0, 0.5) infinite, glow 5s linear infinite;
+	}
+	@keyframes glow {
+		0% {
+			@apply bg-primary-400/50;
+		}
+		33% {
+			@apply bg-secondary-400/50;
+		}
+		66% {
+			@apply bg-tertiary-400/50;
+		}
+		100% {
+			@apply bg-primary-400/50;
+		}
+	}
+	@keyframes pulse {
+		50% {
+			transform: scale(1.5);
+		}
+	}
+</style>
